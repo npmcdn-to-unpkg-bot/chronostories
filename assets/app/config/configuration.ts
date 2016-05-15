e61c01ecfeb6f2ec6e2c43131f580ee0472498ca
fontanf <francesco.fontana@hotelscan.com>
2016-05-16 01:33:22 +0200

    Setting the right database

--- assets/app/config/configuration.ts
+++ assets/app/config/configuration.ts
@@ -13,6 +13,6 @@ export class Configuration {
         expiration: (30 * 24 * 60 * 60 * 1000)
     };
 
-    // public apiBasePath = 'https://aa2016-chronostories.herokuapp.com';
-    public apiBasePath = ''
+    public apiBasePath = 'https://aa2016-chronostories.herokuapp.com';
+    // public apiBasePath = ''
 }
