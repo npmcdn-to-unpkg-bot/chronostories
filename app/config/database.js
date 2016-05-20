ff4ca2708f7f28b8e3da9190472dd8cd6f391f09
fontanf <francesco.fontana@hotelscan.com>
2016-05-20 23:34:09 +0200

    Added env vars for mongo url

--- app/config/database.js
+++ app/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url : 'mongodb://'+process.env.dbuser+':'+process.env.dbpsw+'@ds031802.mlab.com:31802/chronostories_test'
+    url : 'mongodb://'+process.env.dbuser+':'+process.env.dbpsw+'@'+process.env.dburl
     // url : 'mongodb://127.0.0.1:27017/chronostories'
 };
